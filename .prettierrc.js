1eed4629b78dabf82ca98e02da8faf2fab9eabb5
fuchen <284992984@qq.com>
2022-07-19 14:25:41 +0800

    add eslint prettier

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  printWidth: 120,
+  tabWidth: 2,
+  semi: false,
+  singleQuote: true,
+  trailingComma: 'none',
+  arrowParens: 'always',
+  endOfLine: 'auto',
+  htmlWhitespaceSensitivity: 'ignore'
+}
